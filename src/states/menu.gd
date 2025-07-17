7a2e4d0dc8047d555e0a35e11d70b660c37a0b86
KamiNuke <150977125+KamiNuke@users.noreply.github.com>
2025-07-17 18:52:17 +0300

    added basic menu + default scene

---
+++ src/states/menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+var next_scene = preload("res://src/states/default.tscn").instantiate();
+
+func _on_start_button_up() -> void:
+	var tree = get_tree()
+	var cur_scene = tree.get_current_scene()
+	tree.get_root().add_child(next_scene)
+	tree.get_root().remove_child(cur_scene)
+	tree.set_current_scene(next_scene)
+
+
+func _on_exit_button_up() -> void:
+	get_tree().quit();
