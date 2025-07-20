162596cd7d7f551f51677374375a434015e5f64e
KamiNuke <150977125+KamiNuke@users.noreply.github.com>
2025-07-20 09:01:09 +0300

    implemented boost/dodge

--- src/entities/playercontroller.gd
+++ src/entities/playercontroller.gd
@@ -2,10 +2,12 @@ extends CharacterBody3D
 
 @onready var head: Node3D = $"."
 @onready var camera: Camera3D = $head/Camera3D
+@onready var boost_left: Timer = $timers/boost_left
+@onready var boost_cooldown: Timer = $timers/boost_cooldown
 
 var speed
-const WALK_SPEED = 2.5
-const SPRINT_SPEED = 5.0
+const WALK_SPEED = 7.5
+const BOOST_SPEED = 45.0
 
 const JUMP_VELOCITY = 6.5
 const SENSIVITY = 0.005
@@ -17,7 +19,7 @@ var t_bob = 0.0 #don't touch
 
 #FOV
 @export var BASE_FOV = 75.0
-const CHANGE_FOV = 2.5
+const CHANGE_FOV = 1.0
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and !Global.is_paused:
@@ -26,6 +28,8 @@ func _unhandled_input(event: InputEvent) -> void:
 		const LIMIT_VIEW_DOWN = -25
 		const LIMIT_VIEW_UP = 35
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(LIMIT_VIEW_DOWN), deg_to_rad(LIMIT_VIEW_UP))
+
+var i = 1
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -36,10 +40,12 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("space") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	i += 1
 	# Handle sprint
-	if Input.is_action_pressed("shift"):
-		speed = SPRINT_SPEED
-	else:
+	if Input.is_action_just_pressed("shift") and boost_cooldown.is_stopped():
+		boost_left.start()
+		speed = BOOST_SPEED
+	elif boost_left.is_stopped():
 		speed = WALK_SPEED
 
 	# Get the input direction and handle the movement/deceleration.
@@ -52,10 +58,11 @@ func _physics_process(delta: float) -> void:
 			velocity.x = direction.x * speed
 			velocity.z = direction.z * speed
 		else:
-			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
-			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
+			const WHAT_DOES_IT_DO = 2.5
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * WHAT_DOES_IT_DO)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * WHAT_DOES_IT_DO)
 	else:
-		const CONTROL_IN_AIR = 1.0
+		const CONTROL_IN_AIR = 10.0
 		velocity.x = lerp(velocity.x, direction.x * speed, delta * CONTROL_IN_AIR)
 		velocity.z = lerp(velocity.z, direction.z * speed, delta * CONTROL_IN_AIR)
 
@@ -64,7 +71,7 @@ func _physics_process(delta: float) -> void:
 	camera.transform.origin = headbob(t_bob)
 	
 	# FOV
-	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
+	var velocity_clamped = clamp(velocity.length(), 0.5, BOOST_SPEED * 2)
 	var target_fov = BASE_FOV + CHANGE_FOV * velocity_clamped
 	var covered_distance = delta * 8.0
 	camera.fov = lerp(camera.fov, target_fov, covered_distance)
@@ -77,3 +84,11 @@ func headbob(time) -> Vector3:
 	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
 	
 	return pos
+
+func _on_boost_left_timeout() -> void:
+	boost_left.stop()
+	boost_cooldown.start()
+
+
+func _on_boost_cooldown_timeout() -> void:
+	boost_cooldown.stop()
