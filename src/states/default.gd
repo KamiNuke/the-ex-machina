45eddbe7e3b2b43735a61b779f7d5cf4031eec9b
strekecheke <strekecheke@gmail.com>
2025-07-31 15:46:16 +0300

    docs

--- src/states/default.gd
+++ src/states/default.gd
@@ -18,6 +18,7 @@ var revives_left = 2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
